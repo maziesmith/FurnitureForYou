a99d88fa51e8c772cd38e4bcec7004434ac39e01
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-02-12 21:11:12 +0200

    Added Initial WebForms project

---
+++ FFY/FFY.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FFY.Web.Startup))]
+namespace FFY.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
