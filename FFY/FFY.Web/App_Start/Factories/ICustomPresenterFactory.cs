e0fc26039a0b27f8ba185a0b4d13196d18b31181
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-02-13 08:58:54 +0200

    Added Ninject configurations and binding for MVP (credits to shakuu)

---
+++ FFY/FFY.Web/App_Start/Factories/ICustomPresenterFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using WebFormsMvp;
+
+namespace FFY.Web.App_Start.Factories
+{
+    public interface ICustomPresenterFactory
+    {
+        IPresenter GetPresenter(Type presenterType, IView viewInstance);
+    }
+}
---
+++ FFY/FFY.Web/App_Start/NinjectModules/MvpNinjectModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Ninject;
+using Ninject.Activation;
+using Ninject.Extensions.Factory;
+using Ninject.Modules;
+using Ninject.Parameters;
+using WebFormsMvp;
+using WebFormsMvp.Binder;
+using FFY.Web.App_Start.Factories;
+
+namespace FFY.Web.App_Start.NinjectModules
+{
+    public class MvpNinjectModule : NinjectModule
+    {
+        private const string ViewConstructorArgumentName = "view";
+
+        public override void Load()
+        {
+            this.Bind<IPresenterFactory>().To<WebFormsMvpPresenterFactory>().InSingletonScope();
+            this.Bind<ICustomPresenterFactory>().ToFactory().InSingletonScope();
+            this.Bind<IPresenter>()
+                .ToMethod(this.GetPresenter)
+                .NamedLikeFactoryMethod((ICustomPresenterFactory factory) => factory.GetPresenter(null, null));
+        }
+
+        private IPresenter GetPresenter(IContext context)
+        {
+            var parameters = context.Parameters.ToList();
+
+            var presenterType = (Type)parameters[0].GetValue(context, null);
+            var viewInstance = (IView)parameters[1].GetValue(context, null);
+
+            var constructorParameter = new ConstructorArgument(ViewConstructorArgumentName, viewInstance);
+
+            var presenter = (IPresenter)context.Kernel.Get(presenterType, constructorParameter);
+            return presenter;
+        }
+    }
+}
